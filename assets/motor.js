7f9c572243177d135fbb9a786fa772d4e7f9a6f1
Austin Kumbera <austin@petra.localdomain>
2021-11-03 12:34:50 -0600

    Added motors.js. Attempt to import function from js file.

---
+++ assets/motor.js
@@ -0,0 +1,12 @@
+/* motor.js
+ * functions to support a sound simulator
+ * based on the idea of a rotating crankshaft
+ * and distinct chamber comustions.
+ * 2021-11, Austin Kumbera
+ */
+
+function play() {
+  var audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3');
+  audio.play();
+}
+
---
+++ motors.markdown
@@ -0,0 +1,9 @@
+---
+layout: page
+title: "Motor Sounds"
+permalink: /motor-sounds/
+---
+<script src="assets/motors.js"></script> 
+
+<input id="clickMe" type="button" value="clickme" onclick="play()" />
+
